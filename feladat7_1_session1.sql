2423d0f545f59cad294c4751d89546647fab15b1
Nyári Norbert <nyari.norbert.87@gmail.com>
2018-12-10 20:23:26 +0100

    7/1 feladat pipa

---
+++ feladat7_1_session1.sql
@@ -0,0 +1,15 @@
+declare
+    dolg dolgozo%rowtype;
+    dolg2 dolgozo%rowtype;
+    dp dept%rowtype;
+begin
+
+for i in 1..1000000
+loop
+    select * into dolg from dolgozo where dkod = 1 for update;
+
+    select * into dolg2 from dolgozo where dkod = 2 for update;
+    
+    rollback;
+end  loop;  
+end;
